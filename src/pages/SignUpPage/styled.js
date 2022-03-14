028b5ab2eb1f4aa675c2027a03ec801c5ef1340c
Vitorduarte0 <viorsanta.cal.10@gmail.com>
2022-03-14 15:21:25 -0300

    subindo constants e formulario de signup

---
+++ src/pages/SignUpPage/styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+export const ContainerInput = styled.div`
+    display: grid;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    form{
+        display: grid;
+        gap: 1rem;
+        width: 90vw;
+        height: 60vh;
+        
+    }
+
+`
