1c01f31fd4ef460975d8eb42e6a4a9b11d627236
GeysonMes <geysonsousa17@gmail.com>
2022-03-14 20:03:46 -0300

    iniciando pagina feed

---
+++ src/pages/FeedPage/CardFeed/CardFeed.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea, Box } from '@mui/material';
+import styled from 'styled-components'
+import axios from 'axios';
+import { BASE_URL } from '../../../constants/url';
+import useRequestData from '../../../hooks/useRequestData';
+
+const InfoRestaurant = styled(Box)`
+    display: flex;
+    justify-content: space-between
+`
+const CardFeed = () => {
+
+    const [restaurants] = useRequestData([], `${BASE_URL}/restaurants`)
+    const restaurantList = restaurants && restaurants.map((rest) => {
+        return (
+            <div key={rest.id}>
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image={rest.logoUrl}
+                            alt="green iguana"
+                        />
+
+                        <CardContent>
+                            <Typography gutterBottom variant="h6" color="primary" component="div">
+                                {rest.name}
+                            </Typography>
+                            <InfoRestaurant>
+                                <Typography variant="body2" color="text.secondary">
+                                    {rest.deliveryTime} min
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                      (`Frete: R$${rest.shipping},00`)
+                                </Typography>
+                            </InfoRestaurant>
+                        </CardContent>
+
+                    </CardActionArea>
+                </Card>
+            </div>
+        )
+    })
+    return (
+       <div>
+        {restaurantList}
+       </div>
+    );
+}
+export default CardFeed;
---
+++ src/pages/FeedPage/styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+export const Title = styled.div`
+    display: flex;
+    justify-content:center;
+    width: 100%;
+    border-bottom: 1px solid #c4c4c4;
+    margin-bottom: 1px;
+    h2{
+        font-size: 15px;
+        font-weight: 500;
+        
+    }
+`
+
