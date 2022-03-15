7f8b2a80e3a410c82fd3e4c9c14b38b17fbb05ff
AnaKarine27 <karinesantos364@gmail.com>
2022-03-15 15:44:14 -0300

    finalizando renderizacao info restaurante

---
+++ src/pages/RestaurantPage/CardRestaurant/styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+const InfoRestaurant = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+`
+const BoxBotton = styled(Box)`
+display: flex;
+justify-content: space-between;
+`
+const StyleButton = styled.button`
+font-size: 16px;
+padding-top: 8px;
+padding-left: 15px;
+border-color: #6AC453;
+color: #6AC453;
+background: white;
+border-radius: 8px 0px 0px 0px;
+:hover{
+  background-color: #E2FFD5;
+}
+`
