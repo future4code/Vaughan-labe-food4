a5010419c460828e970e25c2565544519f396ad1
Gledson Lucas <gledsonlucas111@gmail.com>
2022-03-15 09:02:37 -0300

    alterações no carPage

--- src/pages/CarPage/CarPage.js
+++ src/pages/CarPage/CarPage.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
+import GlobalStateContext from "../../global/GlobalStateContext";
 
 const CarPage = () => {
+  const {cart, setCart} = useContext(GlobalStateContext)
+
+  
 
   return (
     <div>
-      <h1>CarPage</h1>
+      <h2>Meu carrinho</h2>
+      <div>endereço</div>
+
+      <div>
+        {cart.length === 0? "carrinho vazio": cart.map((i)=>{
+          return<div>{i}</div>
+        })}
+      </div>
+      <div>
+        <h3>frete R$0,00</h3>
+        <div>
+          <h3>sub total:</h3>
+          <h3>R$:0,00</h3>
+        </div>
+       
+       <div>
+         <h3>Formas de pagamento</h3>
+         <p>Dinheiro</p>
+         <p>Cartão de crédito</p>
+       </div>
+       
+       
+
+      </div>
+      <div>nav bar</div>
     </div>
   )
 }
