7e120d43d5ba27e0612dfbb23e7273e60a427731
Vitorduarte0 <viorsanta.cal.10@gmail.com>
2022-03-16 02:04:11 -0300

    subindo requisição de pegar perfil, historico de pedidos e começamdo a estilização da pagina de perfil

---
+++ src/pages/ProfilePage/CardProfile/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+export const ContainerInfoProfile = styled.div `
+    display: flex;
+    flex-direction: column;    
+    align-items: center;
+    justify-content: flex-start;
+`
+export const Ellen = styled.div `
+    background-color: #b8b8b8;
+    
+`
--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,13 +1,52 @@
 import React from "react";
 import NavBarProfile from "../../components/NavBar/NavBarPorfile";
-
+import useRequestData from "../../hooks/useRequestData"
+import {BASE_URL} from "../../constants/url"
+import { ContainerProfile, ContainerInfoProfile, ContainerImgProfile, Ellen } from "./styled";
+import edit from "../../assets/edit@2x.png"
+import CardProfile from "./CardProfile/CardProfile";
 const ProfilePage = () => {
-
+  const ordersHistory = useRequestData([], `${BASE_URL}/orders/history`)
+  const getProfile = useRequestData([], `${BASE_URL}/profile`)
+  console.log(ordersHistory && ordersHistory)
+  console.log(getProfile)
+  const ellenMinhaVida = getProfile?.map((profile)=>{
+    return(
+      <>
+      <CardProfile
+      name={profile.user?.name}
+      profile={profile.user?.email}
+      cpf={profile.user?.cpf}
+      address={profile.user?.address}
+      />
+      </>
+    )
+  })
   return (
-    <div>
-      <h1>ProfilePage</h1>
-      <NavBarProfile/>
-    </div>
+    <ContainerProfile>
+      <h3 className="myCart">Meu Perfil</h3>
+      <ContainerInfoProfile>
+        <ContainerImgProfile>
+      <img src={edit} alt={"Imagem de editar"} />
+      </ContainerImgProfile>
+      {ellenMinhaVida}
+      {/* {getProfile && getProfile.map((profile)=>{
+          return(
+            <>
+            <p key={profile.id}>{profile.user?.name}</p>
+            <p key={profile.id}>{profile.user?.email}</p>
+            <p key={profile.id}>{profile.user?.cpf}</p>
+            <Ellen>
+            <h4>Endereço cadastro</h4>
+            <p key={profile.id}>{profile.user?.address}</p>
+            </Ellen>
+            </>
+          )
+      })} */}
+      
+      </ContainerInfoProfile>
+      <NavBarProfile/>  
+    </ContainerProfile>
   )
 }
 
