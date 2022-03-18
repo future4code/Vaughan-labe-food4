c351ec4264fb28164745e280fb0422b924df7c0b
AnaKarine27 <karinesantos364@gmail.com>
2022-03-18 12:25:27 -0300

    estilizacao

--- src/pages/SignUpPage/styled.js
+++ src/pages/SignUpPage/styled.js
@@ -10,27 +10,39 @@ export const ContainerInput = styled.div`
         gap: 1rem;
         width: 90vw;
         height: 60vh;
-        
+    }
+    p{
+        font-size: 16px;
+        text-align: center;
     }
 `
-
 export const ContairnerImg = styled.div`
     display: grid;
     justify-content: center;
     align-items: center;
     margin: 10px;
+    margin-top: 65px;
     img{
         display: grid;
         justify-content: center;
         align-items: center;
+        width: 104px;
     }
-
 `
-
-// export const Img = styled.img`
-//     display: grid;
-//     justify-content: center;
-//     align-items: center;
-//     margin-left: 4rem;
-
-// `
+export const ContainerField = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+`
+export const Button = styled.button`
+    font-size: 16px;
+    font-weight: 500;
+    background-color: #5cb646;
+    border: none;
+    border-radius: 4px;
+    height: 42px;
+    width: 340px;
+:hover{
+    background-color: #A2E884;
+}
+`
